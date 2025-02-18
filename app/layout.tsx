4e9b201255d812e7a3ca7d2ed84fe9a8e0de8a95
Ishant0985 <yadav.ishantsbi0985@gmail.com>
2025-02-18 20:05:39 +0530

    added ads and seo in layout.tsx

--- app/layout.tsx
+++ app/layout.tsx
@@ -5,6 +5,8 @@ import './globals.css'
 import FirebaseAuthProvider from "@/components/FirebaseAuthProvider" // adjust the path if needed
 import Navbar from '@/components/Navbar'
 import { ThemeProvider } from "@/components/theme-provider"
+import { SpeedInsights } from "@vercel/speed-insights/next"
+import { Analytics } from "@vercel/analytics/react"
 export default function RootLayout({
   children,
 }: {
@@ -13,6 +15,12 @@ export default function RootLayout({
   return (
     <ClerkProvider>
       <html lang="en">
+        <head>
+        <meta name="google-adsense-account" content="ca-pub-9163676450980454"/>
+        <meta name="google-site-verification" content="cAK8SSwBYFev4YHkUZy48fJGnA5_ndWSnnyz3wY69UY" />
+        <script async src="https://pagead2.googlesyndication.com/pagead/js/adsbygoogle.js?client=ca-pub-9163676450980454"
+     crossOrigin="anonymous"></script>
+          </head>
         <body>
           <ThemeProvider
             attribute="class"
@@ -21,6 +29,8 @@ export default function RootLayout({
             disableTransitionOnChange
           >
             <Navbar/>
+            <Analytics/>
+            <SpeedInsights/>
             <FirebaseAuthProvider>
             {children}
             </FirebaseAuthProvider>
